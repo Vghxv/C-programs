bde08814c6f56afea504e17df9c87a9cf3adbad7
Vincent <hankggg2003xd@gmail.com>
2022-01-31 17:28:04 +0800

    update

--- practice6.c
+++ practice6.c
@@ -4,9 +4,9 @@ int fun(int a,int b,int c){
 }
 int main(void){
     int i=0,j=0;
-    char str[20];//定義一個最大長度為19，末尾是'\0'的字元陣列來儲存字串
+    char str[20];
     printf("請輸入一個字串：");
-    fgets(str,7,stdin);//從輸入流stdin即輸入緩衝區中讀取7個字元到字元陣列str中
+    fgets(str,7,stdin);
     printf("%s",str);
 
     return 0;
---
+++ userinput.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    char b[5];
+
+    scanf("%d",&a);
+    printf("%d\n",a);
+    scanf("%s",&b);
+    printf("%s\n",b);
+    return 0;
+}
