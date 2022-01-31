f311ed7646781d7022a2015989f29f392a6b01e9
Vincent <hankggg2003xd@gmail.com>
2022-02-01 01:06:28 +0800

    night

--- userinput.c
+++ userinput.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
 int main(){
-    int a;
-    char b[5];
+    int age;
+    char fuck[5];
+    char id[15];
+    char name[25];
+    char nickname[10];
+    printf("Enter your age\n");
+    scanf("%d",&age);
+    printf("age : %d\n",age);
 
-    scanf("%d",&a);
-    printf("%d\n",a);
-    scanf("%s",&b);
-    printf("%s\n",b);
+    fgets(fuck,5,stdin);
+
+    printf("Enter your name\n");
+    fgets(name,25,stdin);
+    name[strlen(name)-1]='\0';
+    printf("name : %s\n",name);
+
+    printf("Enter your ID\n");
+    fgets(id,15,stdin);
+    printf("ID : %s\n",id);
+    id[strlen(id)-1]='\0';
+
+    printf("Enter your niclname\n");
+    fgets(nickname,10,stdin);
+    printf("nickname : %s\n",nickname);
+
+
     return 0;
 }
