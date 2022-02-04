81881a9652204ca0089875cc74b2d9a5833c2fac
unknown <hankggg2003xd@gmail.com>
2022-02-05 01:57:14 +0800

    caculator

---
+++ caculator.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    char op;
+    double a=0;
+    double b=0;
+    double ans=0;
+    printf("caculator on\n");
+    scanf("%lf %c %lf",&a,&op,&b);
+    switch(op){
+        case '+':
+            ans=a+b;
+            break;
+        case '-':
+            ans=a-b;
+            break;
+        case '*':
+            ans=a*b;
+            break;
+        case '/':
+            ans=a/b;
+            break;
+        default:
+            printf("incorrect input");
+            break;
+        
+    }
+    printf("%.3lf",ans);
+
+    return 0;
+}
