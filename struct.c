9d440ec6b5bef08b9c5eb3fa635fe6ef8a7f3da4
Vincent <hankggg2003xd@gmail.com>
2022-02-22 17:02:28 +0800

    2/22 end

--- struct.c
+++ struct.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 struct Node{
-    int value;
+    int val;
     int *next;
 };
 int main(){
@@ -8,13 +8,16 @@ int main(){
     struct Node n2;
     int tmp=0;
     scanf("%d", tmp);
-    n1.value=tmp;
-    getchar();
-
+    n1.val=tmp;
+    //getchar();
+    printf("-------");
     scanf("%d",tmp);
-    n2.value=tmp;
-    n1.next=&n2.value;
-    getchar();
+    n2.val=tmp;
+    n1.next=&n2.val;
+    //getchar();
 
+    printf("values : %d%d",n1.val,n2.val);
+    printf("pointer : %d",n1.next);
+    printf("%d",&n2.val);
     return 0;
 }
