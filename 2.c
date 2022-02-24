a2b2019a972a21b9188f988642daaa420fe575d3
Vincent <hankggg2003xd@gmail.com>
2022-02-24 23:16:31 +0800

    2 good

--- 2.c
+++ 2.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
-
+void f() {}
 int main(){
     int i;
-    int status=1;
-
-    char input[5]={0};
+    int state=0;
+    int outn=0;
+    char input='0';
     for(i=0;i<5;++i){
-        scanf("%c%*c",&input[i]);
-        if()
-            status<<;
-    }
-    for(i=0;i<5;++i){
-        printf("%c\n",input[i]);
+        scanf("%c%*c",&input);
+        if(input=='O'){
+            outn++;
+            if(outn==3)state=0;
+        }
+        else if(input=='1')state=(state<<1)|(1);
+        else if(input=='2')state=(state<<2)|(1<<1);
+        else if(input=='3')state=(state<<3)|(1<<2);
+        else f();
+        state=state&7;
     }
 
+    printf("%d\n", state&1);
+    printf("%d\n", (state>>1)&1);
+    printf("%d", (state>>2)&1);
     return 0;
 }
