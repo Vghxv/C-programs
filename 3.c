c1a2d06b17d2c4556d6585a64e23bdb5c35a5b9c
unknown <hankggg2003xd@gmail.com>
2022-02-25 18:07:30 +0800

    3done

--- 3.c
+++ 3.c
@@ -4,11 +4,13 @@ int main(){
     int m2;
     float x;
     float y;
-    scanf("%d",&m1);
-    scanf("%d",%m2);
-    scanf("%f",%x);
-    scanf("%f",%y);
-
+    scanf(" %d",&m1);
+    scanf( "%d",&m2);
+    scanf(" %f",&x);
+    scanf(" %f",&y);
+    printf("%.1f\n",(m1+m2)*x*0.92-y*0.05);
+    printf("%.1f\n",y*0.05);
+    printf("%.1f",(m1+m2)*x*0.08);
 
     return 0;
 }
