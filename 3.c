9aa0263e92e4ffab14e847119ee94c52c50e4ab8
Vincent <hankggg2003xd@gmail.com>
2022-02-25 13:12:00 +0800

    2/25

---
+++ 3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int m1;
+    int m2;
+    float x;
+    float y;
+    scanf("%d",&m1);
+    scanf("%d",%m2);
+    scanf("%f",%x);
+    scanf("%f",%y);
+
+
+    return 0;
+}
