527d678a849401d43a76606f31e70a9cde782ce2
Vincent <hankggg2003xd@gmail.com>
2022-03-17 19:15:56 +0800

    3/17

--- operator.c
+++ operator.c
@@ -4,11 +4,11 @@ int main(){
     int b=6;
     int c=0;
     int d=0;
-    /*c=a + b++;//14
+    c=a++ + b++;//14
     printf("%d\n",a);
     printf("%d\n",b);
     printf("%d\n",c);
-    printf("%d\n",d);*/
+    printf("%d\n",d);
     d=a-- - --b;//2
     printf("%d\n",a);
     printf("%d\n",b);
