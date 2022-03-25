b9b21baa052b738d1822074f68d95182fe33f117
Vincent <hankggg2003xd@gmail.com>
2022-03-25 15:15:46 +0800

    rename

---
+++ structqueue.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int val;
+    struct node* next;
+};
+struct queue{
+    struct node* head;
+    struct node* tail;
+};
+void push(struct queue* n,int value){
+    struct node t = malloc(sizeof(struct node);
+    n->tail->val = value;
+    n->tail->next = t;
+}
+void pop(struct queue* n,int value){
+	struct node t = malloc(sizeof(struct node);
+	t->next = n->head->next;
+	n->head->next = n->head->next->next;
+	free(t);
+}
+int main(){
+	
+    return 0;
+}
