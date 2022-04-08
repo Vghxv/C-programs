26101a3caed4536ae2761c30faf560c31637fa50
Vincent <hankggg2003xd@gmail.com>
2022-04-08 14:41:50 +0800

    fab undone

--- fabt.c
+++ fabt.c
@@ -2,20 +2,21 @@
 #include<stdlib.h>
 #include<string.h>
 
+int f(int *arr,int n)return(!arr[n]&&n>1)?f(arr,n-1)+f(arr,n-2):arr[n];
 int f(int *arr,int n){
-	if(n<=2)return 1;
-	if(arr[n-1]!=0)return arr[n-1];
-	else arr[n-1]=f(arr,n-1);
-	if(arr[n-2]!=0)return arr[n-2];
-	else arr[n-2]=f(arr,n-2);
-	return arr[n-1]+arr[n-2];
+	if(!arr[n]&&n>1){
+		return f(arr,n-1)+f(arr,n-2);
+	}
+	return arr[n];
 }
 int main(){
 	int n;
 	int *arr;
-	scanf("%d",&n);
+	scanf("%d%*c",&n);
 	arr=malloc(n*sizeof(int));
 	memset(arr,0,n*sizeof(int));
-	printf("%d",f(arr,n));
+	arr[0]=1;arr[1]=1;
+	printf("%d",f(arr,n-1));
+	free(arr);
 	return 0;
 }
