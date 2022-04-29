8bb917519445b1033dba40cd4c88c144c692947a
Vincent <hankggg2003xd@gmail.com>
2022-04-29 18:00:33 +0800

    d

--- 21.c
+++ 21.c
@@ -1,57 +1,108 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#define wordlen 30
+#define wordlen 50
 #define ROW 10 
+
 typedef char(*_2dlist)[wordlen];
-void debug(_2dlist mem){
-	int i,j;
-	for(i=0;i<ROW;i++){
-		for(j=0;j<wordlen;j++){
-			printf("%c",mem[i][j]);
-		}
-		puts("");
-	}
-}
-int iscomplement(_2dlist a,char *b){
+typedef char(*_2dlistB)[wordlen*2];
+// _2dlist init(int row){
+	// int i;
+	// _2dlist k=(_2dlist)malloc(row*wordlen*sizeof(char));
+	// for(i=0;i<row;i++)memset(k[i],'#',sizeof(k[i]));
+	// return k;
+// }
+void debug(char *a){
 	int i;
-	puts(b);
-	for(i=0;i<ROW;i++){
-		if(strcmp(*(a+i),b)==0)return 0;
+	for(i=0;i<wordlen;i++){
+		printf("%d ",*(a+i));
 	}
-	return 1;
+	printf("\n");
+}
+_2dlistB initB(int row){
+	int i;
+	_2dlistB k=(_2dlistB)malloc(row*wordlen*2*sizeof(char));
+	for(i=0;i<row;i++)memset(k[i],'#',sizeof(k[i]));
+	return k;
+}
+
+// void split(char *a,const char *delim,_2dlist b){
+	// char *token = strtok(a,delim);
+	// int i=0;
+	// while(token!=NULL){
+		// strncpy(*(b+i++),token,wordlen);
+		// token=strtok(NULL,delim);
+	// }
+// }
+// void debug(_2dlist a,int row){
+	// int i,j;
+	// for(i=0;i<row;i++){
+		// for(j=0;j<wordlen;j++){
+			// printf("%c",a[i][j]);
+		// }
+		// puts("");
+	// }
+// }
+int iscomplement(char *a,char *b,char *c){
 }
 int main(){
-	char str[100],strtmp[100];
+	char str[wordlen*2];
+	char tmp[wordlen];
 	memset(str,0,sizeof(str));
-	memset(strtmp,0,sizeof(strtmp));
-	char *tok,*toktmp;
-	int n,i,total=0;
-	
+	int n,i,j,total=0;
 	gets(str);
-	_2dlist mem =(_2dlist)malloc(ROW*wordlen*sizeof(char));
-	for(i=0;i<ROW;i++)memset(mem[i],'#',sizeof(mem[i]));
-	
-	i=0;
-	tok=strtok(str," ");
-	while(tok!=NULL){
-		strncpy(*(mem+i++),tok,wordlen);
-		tok=strtok(NULL," ");
-	}
-	debug(mem);
-	
+	printf("str:%s\n",str);
+	// _2dlist S = init(ROW);
+	// split(str," ",S);
+	// debug(S,ROW);
 	scanf("%d%*c",&n);
+	_2dlistB sav = initB(n);
 	for(i=0;i<n;i++){
-		gets(strtmp);
-		toktmp=strtok(strtmp," ");
-		while(toktmp!=NULL){
-			if(iscomplement(mem,toktmp)){
-				total++;
+		memset(tmp,0,sizeof(wordlen*2));
+		gets(tmp);
+		strncpy(sav[i],tmp,wordlen*2);
+	}
+	// printf("str:%s------\n",str);
+	debug(str);
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+		// printf("str:%s------\n",str);
+		// if(iscomplement(sav[i],sav[j],str))total++;
+		// int k;
+		char *toka,*tokb,*tokc;
+		int founded;
+		tokc=strtok(str," ");
+		debug(str);
+		
+		printf("sav[i]:%s\n",sav[i]);
+		printf("sav[j]:%s\n",sav[j]);
+		printf("str:%s\n",str);
+		while(tokc!=NULL){
+			founded=0;
+			toka = strtok(sav[i]," ");
+			while(toka!=NULL){
+				if(strncmp(toka,tokc,50)==0)founded=1;
+				tokb = strtok(sav[j], " ");
+				while(tokb!=NULL){
+					printf("tokc : %s\n",tokc);
+					printf("toka : %s\n",toka);
+					printf("tokb : %s\n",tokb);
+					if(strncmp(tokb,tokc,50)==0)founded=1;
+					if(strncmp(toka,tokb,wordlen*2)==0)return 0;
+					tokb=strtok(NULL," ");
+				}
+				toka=strtok(NULL," ");
 			}
-			toktmp=strtok(NULL," ");
+			// if(founded!=1)return 0;
+			puts("----tokcnew------");
+			tokc=strtok(NULL," ");
+			// puts("---------------");
+		}
+		total+=founded;
+			// printf("%d\n",total);
 		}
 	}
-	free(mem);
+	free(sav);
 	printf("%d",total);
 	return 0;
 }
