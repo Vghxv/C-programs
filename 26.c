3ee6e9a6043c55697551127c88daf9a0ff7904d0
Vincent <hankggg2003xd@gmail.com>
2022-05-10 22:31:24 +0800

    26d

--- 26.c
+++ 26.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 #include<math.h>
 double f1(double a,double x){
-	return sqrt(fabs(a-pow(x,2)));
+	return fabs(sqrt(fabs(a-pow(x,2))));
 }
 double f2(double a,double x){
-	return (a*pow(x,3) + 7.0L*x)/sqrt(fabs(a+x));
+	return fabs((a*x*x*x + 7.0L*x)/sqrt(fabs(a+x)));
 }
 double integral(int n,double a,double p, double q,double (*f)(double,double)) {
 	int i;
-	double area = (fabs(((*f)(a,p)) + fabs((*f)(a,q)))) / 2.0L;
-	double gap = (q - p) / n;
+	double area = ((*f)(a,p) + (*f)(a,q)) / 2.0L;
+	double gap = (q - p) / (double)n;
 	double next = p;
 	for (i = 1; i < n; i++) {
-		area += fabs((*f)(a,next+=gap));
+		next+=gap;
+		area += fabs((*f)(a,next));
 	}
 	return area * gap;
 }
 int main(){
-	
 	int jg,err,n;
 	double a,p,q;
 	double(*f)(double,double);
@@ -29,7 +29,7 @@ int main(){
 		err*=-1;
 		f=(jg==1)?f1:f2;
 		double errn=pow(10,err);
-		double parea=0.0L;
+		double parea=1.0L;
 		double narea=0.0L;
 		while(1){
 			narea=integral(n,a,p,q,f);
