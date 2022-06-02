7fff38a8fa62e51c97219aeaa5b37a4f8161e999
Vincent <hankggg2003xd@gmail.com>
2022-06-02 12:14:46 +0800

    6/2

---
+++ 35.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+typedef struct llld_s{
+	char num[130];
+	int sign;
+	int dp;
+}llld_t;
+llld_t* loadLFP{
+	llld_t k;
+	
+	return k;
+}
+int main(){
+	gets(a);
+	gets(b);
+	
+	return 0;
+}
